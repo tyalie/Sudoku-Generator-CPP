50f66bf177dcab59472353678920cf4e896ba892
gansgar <ansgar.f26@gmail.com>
2016-06-26 15:02:31 -0400

    - added SudokuSolver - made compilable for linux and OS X

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <QApplication>
+#include <iostream>
+
+#include <list>
 
 #include "GUI/mainwindow.h"
+#include "Generator/GenSudoku.h"
+#include "Generator/SudokuSolver.h"
 
 int main(int argc, char *argv[]) {
-    int mode = 0;
-    
+    int mode = 1;
+
     if(mode == 0) {
         QApplication a(argc, argv);
         MainWindow w;
@@ -14,7 +19,17 @@ int main(int argc, char *argv[]) {
 
         return a.exec();
     } if(mode == 1) {
+        /*GenSudoku s;
+        //s.Sudoku::setAtIndex(1,1);
+        std::cout << s.toString() << std::endl;
+
+        //std::list<GenSudoku> ret = s.expand();
+        //for(std::list<GenSudoku>::iterator i=ret.begin(); i!=ret.end(); i++)
+        //    std::cout<< i->toString() << std::endl;
+
+        SudokuSolver::DFSLV(s, 2);
+        std::cout << SudokuSolver::getLastField().toString() << std::endl;*/
     }
-    
+
     return 0;
 }
